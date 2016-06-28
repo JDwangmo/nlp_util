5dcfb79daa73a554b28d293c09f916bfd5196663
jdwang <383287471@qq.com>
2016-06-28 19:54:48 +0800

    update Corpus

--- README.md
+++ README.md
@@ -20,9 +20,9 @@
     - describe: jieba分词
     - 依赖包: jieba 0.38
     - 项目结构:
-        - stopword.txt: 中文停止词
+        - stopword.txt: 中文停止词表.
         - userdict.txt: 用户自定义字典
-        - jieba_util.py: jieba分词工具类,自封装了一层,内含 Jieba_Util类
+        - jieba_util.py: jieba分词工具类,自封装了一层,即包装成一个 Jieba_Util类,这个类主要在原 jieba 分词的基础上对增加类，用户字典，是否移除stopwords，是否统一替换数字 等功能.
         
     
     
@@ -31,9 +31,12 @@
 
 #### [CNN类](https://github.com/JDwangmo/nlp_util/tree/master/deep_learning/cnn/)
 - [cnn/randEmbedding_cnn:]
-    - describe: 基于CNN-rand,对输入层增加了dropout rate的调节,目的在于避免输入特征过多,训练时间长,另外可以设置k-max,不止原paper中模型的1-max.随机词向量输入,一层CNN,多种卷积核,具体见:[Kim,Convolutional Neural-Networks for Sentence Classification,2014](https://github.com/JDwangmo/coprocessor#2convolutional-neural-networks-for-sentence-classification).
+    - describe: 
+    > 基于CNN-rand,对输入层增加了dropout rate的调节,目的在于避免输入特征过多,训练时间长,另外可以设置k-max,不止原paper中模型的1-max.随机词向量输入,一层CNN,多种卷积核,具体见:[Kim,Convolutional Neural-Networks for Sentence Classification,2014](https://github.com/JDwangmo/coprocessor#2convolutional-neural-networks-for-sentence-classification).
     - 依赖包: Keras 1.0.4
     - 项目结构:
-        - [feature_encoder.py]:特征编码类,将原始输入的句子转换为补齐的字典索引的形式,使用0补长.
-        - [randEmbedding_cnn_model.py]:CNN-rand模型类,搭建一层卷积层的CNN-rand.集成了训练,测试等方法.关于这个模型的具体内容请参考: [Kim et al.,Convolutional Neural-Networks for Sentence Classification,EMNLP 2014](https://github.com/JDwangmo/coprocessor#2convolutional-neural-networks-for-sentence-classification)
+        - [feature_encoder.py]: 特征编码类,将原始输入的句子转换为补齐的字典索引的形式,使用0补长.
+        - [randEmbedding_cnn_model.py]:
+            > CNN-rand模型类,搭建一层卷积层的CNN-rand.集成了训练,测试等方法.
+            > 关于这个模型的具体内容请参考: [Kim et al.,Convolutional Neural-Networks for Sentence Classification,EMNLP 2014](https://github.com/JDwangmo/coprocessor#2convolutional-neural-networks-for-sentence-classification)
     
--- data_processing_util/jiebanlp/jieba_util.py
+++ data_processing_util/jiebanlp/jieba_util.py
@@ -1,4 +1,4 @@
-#encoding=utf8
+# encoding=utf8
 __author__ = 'jdwang'
 __date__ = 'create date: 2016-05-15'
 import numpy as np
@@ -10,60 +10,55 @@ import os
 import re
 import io
 
-# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
-
-
-
-
 
 class Jieba_Util(object):
+    '''
+        the wrapper of jieba tool.
+        对jieba分词进行一层包装.
+
+    '''
+
     def __init__(self,
-                 verbose = 0):
+                 verbose=0):
         '''
             1. 初始化参数
             2. 加载用户字典和stop word列表
-        '''
+        :param verbose: 数值越大，打印越多的详细信息，设置为0时，什么信息都不显示.
+        :type verbose: int
 
+        '''
+        # 初始化参数
         self.verbose = verbose
+        # 设置jieba分词对线程
+        jieba.enable_parallel(10)
+
+
 
         # -------------- region start : 2. 加载用户字典和stop word列表 -------------
-        if verbose > 1 :
+        if verbose > 1:
             logging.debug('-' * 20)
             print '-' * 20
             logging.debug('2. 加载用户字典和stop word列表')
             print '2. 加载用户字典和stop word列表'
         # -------------- code start : 开始 -------------
 
-        jieba.load_userdict(os.path.dirname(__file__) + '/userdict.txt')
-        # -------------- print start : just print info -------------
-        if verbose > 1 :
-            logging.debug('=' * 20)
-            logging.debug('加载stopwords列表....')
-            print '加载stopwords列表....'
-        # -------------- print end : just print info -------------
 
+
+        jieba.load_userdict(os.path.dirname(__file__) + '/userdict.txt')
         self.stopword_list = io.open(os.path.dirname(__file__) + '/stopword.txt', 'r',
                                      encoding='utf8').read().strip().split()
         self.exclude_word_list = set(['886', '88'])
-        # -------------- print start : just print info -------------
-        if verbose > 1 :
-            logging.debug(u'stopwords有：%s' % (','.join(self.stopword_list)))
-            print u'stopwords有：%s' % (','.join(self.stopword_list))
-            logging.debug(u'exclude words有：%s' % (','.join(self.exclude_word_list)))
-            logging.debug('=' * 20)
-        # -------------- print end : just print info -------------
+
 
         # -------------- code start : 结束 -------------
-        if verbose > 1 :
+        if verbose > 1:
             logging.debug('-' * 20)
             print '-' * 20
         # -------------- region end : 2. 加载用户字典和stop word列表 ---------------
 
 
-        jieba.enable_parallel(10)
 
-
-    def pos_seg(self,word):
+    def pos_seg(self, word):
 
         # for items in jseg.lcut(word):
         #     print items.flag,items.word
@@ -71,45 +66,49 @@ class Jieba_Util(object):
         return jseg.lcut(word)[0].flag
 
     def seg(self,
-            sentence,sep=' ',
-            full_mode = False,
-            remove_stopword = False,
-            replace_number = False,
+            sentence,
+            sep=' ',
+            full_mode=False,
+            remove_stopword=False,
+            replace_number=False,
             ):
-        '''
-        使用jieba分词进行分词
+        """
+            使用 jieba 分词进行分词
+
         :param sentence: 待分词句子
         :type sentence: str
-        :param remove_stopword: 是否去除stopword
+        :param sep: 将句子分完词之后使用什么字符连接，默认以空格连接.
+        :type sep: str
+        :param full_mode: jieba设置选项，是否使用full mode分词模式.
+        :type full_mode: bool
+        :param remove_stopword: 是否去除 stop word
         :type remove_stopword: bool
-        :param replace_number: 是否把数字替换成字符 NUM
+        :param replace_number: 是否把数字统一替换成字符 NUM
         :type replace_number: bool
-        :return:返回分词后字符串,seg_srt
+        :return: 返回分词后字符串,seg_srt
         :rtype: str
-        '''
-        # logging.debug('是否去除stopwords：%s'%remove_stopword)
-        # for items in jseg.lcut(sentence):
-        #     print items.flag,items.word
 
+        """
 
         seg = []
+        # 数字对模式匹配
         pattern = re.compile('[0-9]+$')
         for items in jseg.lcut(sentence):
             # print items.flag
             # 利用词性标注去除标点符号
             if items.flag in ['x']:
-                logging.debug(u'句子（%s）将标点符号："%s"替换成""'%(sentence,items.word))
+                logging.debug(u'句子（%s）将标点符号："%s"替换成""' % (sentence, items.word))
                 seg.append('')
-                # continue
+
             elif remove_stopword and items.word in self.stopword_list:
-                logging.debug(u'句子（%s）去除stopwords：%s' % (sentence,items))
+                logging.debug(u'句子（%s）去除stopwords：%s' % (sentence, items))
                 continue
             # 将数字替换成 NUM
             elif pattern.match(items.word) and items.word not in self.exclude_word_list:
                 if replace_number:
-                    if self.verbose>1:
-                        logging.debug(u'句子（%s）将数字："%s" 替换成标记："NUMBER"'%(sentence,items.word))
-                        print(u'句子（%s）将数字："%s" 替换成标记："NUMBER"'%(sentence,items.word))
+                    if self.verbose > 1:
+                        logging.debug(u'句子（%s）将数字："%s" 替换成标记："NUMBER"' % (sentence, items.word))
+                        print(u'句子（%s）将数字："%s" 替换成标记："NUMBER"' % (sentence, items.word))
                     seg.append('NUMBER')
                 else:
                     seg.append(items.word)
@@ -123,14 +122,15 @@ class Jieba_Util(object):
         # print sentence
         seg_list = jieba.lcut(sentence, cut_all=full_mode)
         # print seg_list
-        seg_list = [item for item in seg_list if len(item.strip())!=0]
+        seg_list = [item for item in seg_list if len(item.strip()) != 0]
         # print seg_list
         seg_srt = sep.join(seg_list)
         return seg_srt
 
 
-
 if __name__ == '__main__':
+    # 使用样例
+    jieba_util = Jieba_Util()
     sent = u'买手机啊........'
     # sent = u'k好,'
     # print seg(sent,sep='|',full_mode=False,remove_stopword=True)
@@ -138,4 +138,4 @@ if __name__ == '__main__':
     # print seg(sent,sep='|',full_mode=False,remove_stopword=True)
     # sent = u'妈B'
     print ','.join(jieba.cut(sent))
-    print seg(sent,sep='|',full_mode=False)
+    print jieba_util.seg(sent, sep='|', full_mode=False)
