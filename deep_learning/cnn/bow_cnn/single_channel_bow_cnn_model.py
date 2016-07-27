01e365ef7ab90518fcf52d5df26c99990a1af0c0
jdwang <383287471@qq.com>
2016-07-27 13:42:54 +0800

    add get_layer_output

--- deep_learning/cnn/bow_cnn/single_channel_bow_cnn_model.py
+++ deep_learning/cnn/bow_cnn/single_channel_bow_cnn_model.py
@@ -292,10 +292,10 @@ class SingleChannelBowCNN(CnnBaseClass):
                                     [layer1, l1_conv_filter_type[2], -1, 'valid', (k[0], 1), 0.,'relu', 'none'],
                                 ],
                                 l2_conv_filter_type=[
-                                    [layer2, l2_conv_filter_type[0], -1, 'valid', (k[1], 1), 0.,'relu', 'batch_normalization']
+                                    [layer2, l2_conv_filter_type[0], -1, 'valid', (k[1], 1), 0.25,'relu', 'batch_normalization']
                                 ],
-                                full_connected_layer_units=[(hidden1,0.,'relu', 'batch_normalization'),
-                                                            (hidden2,0.,'relu', 'none')],
+                                full_connected_layer_units=[(hidden1,0.5,'relu', 'batch_normalization'),
+                                                            (hidden2,0.5,'relu', 'none')],
                                 nb_epoch=nb_epoch,
                                 earlyStoping_patience=50,
                                 optimizers='sgd',
